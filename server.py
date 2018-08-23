92239d59bbb8836868d22bc26460bc128adf3185
Doram13 <1991doram@gmail.com>
2018-08-23 13:46:37 +0200

    Add Answer feature added

--- server.py
+++ server.py
@@ -29,6 +29,16 @@ def display_question(question_id=None):
     return render_template('display-question.html', id_=question_id)
 
 
+@app.route('/question/<int:question_id>/new-answer')
+def new_answer(question_id=None):
+    return render_template('add-answer.html', id_=question_id)
+
+
+@app.route('/new-answer', methods=['POST'])
+def save_answer():
+    answer = request.form['Answer']
+    return redirect('/')
+
 if __name__ == "__main__":
     app.run(
       debug=True,
