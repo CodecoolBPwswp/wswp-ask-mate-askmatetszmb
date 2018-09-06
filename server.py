4ce3c3639a7740d75f52b14fe70f00d4935abd0c
Bence Bachraty <bakybence@gmail.com>
2018-09-06 14:21:30 +0200

    fixed a typo

--- server.py
+++ server.py
@@ -38,7 +38,7 @@ def display_question(question_id=None):
     answers = datamanager.get_answers(question_id)
     comments = datamanager.get_question_comment(question_id)
     answer_ids = datamanager.get_answer_id(question_id)
-    if answer_ids is []:
+    if answer_ids == []:
         return render_template('display-question.html', question=question, answers=answers, comments=comments)
     else:
         answer_comments = datamanager.get_answer_comment(answer_ids)
